832b7b83149775882c909455b843f52fa0ff61f1
RajeshSharma-9616 <108452276+RajeshSharma-9616@users.noreply.github.com>
2022-07-02 15:26:31 +0530

    Add files via upload
    Leet Code 26. Remove Duplicates from Sorted Array

---
+++ RemoveDuplicate_26.java
@@ -0,0 +1,19 @@
+package com.sharma.ds;
+/*
+Input: nums = [1,1,2]
+Output: 2, nums = [1,2,_]
+Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+ */
+public class RemoveDuplicate_26 {
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 2, 2, 2, 2, 2, 2, 2, 2};
+        int c = 1;
+        for (int i = 1; i < nums.length; i++)
+            if (nums[i] != nums[i - 1]) {
+                nums[c] = nums[i];
+                c++;
+            }
+    }
+}
+
